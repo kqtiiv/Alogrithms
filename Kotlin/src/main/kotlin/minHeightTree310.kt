f58f202f106a640be683b7455a2411a4afe01197
kqtiiv <katiewan.kw@gmail.com>
2026-03-07 20:47:27 +0000

    Add bfs tree questin

---
+++ Kotlin/src/main/kotlin/minHeightTree310.kt
@@ -0,0 +1,34 @@
+// leetcode 310
+
+fun findMinHeightTrees(
+    n: Int,
+    edges: Array<IntArray>,
+): List<Int> {
+    if (n == 1) return listOf(0)
+    val adj = Array<MutableList<Int>>(n) { mutableListOf() }
+    val outbound = IntArray(n) { 0 }
+    for ((u, v) in edges) {
+        adj[u] += v
+        adj[v] += u
+        outbound[u]++
+        outbound[v]++
+    }
+
+    val q = ArrayDeque<Int>()
+    for (i in adj.indices) {
+        if (outbound[i] == 1) q += i
+    }
+
+    var processed = 0
+    while (n - processed > 2) {
+        processed += q.size
+        repeat(q.size) {
+            val x = q.removeFirst()
+            for (y in adj[x]) {
+                outbound[y]--
+                if (outbound[y] == 1) q += y
+            }
+        }
+    }
+    return q.toList()
+}
